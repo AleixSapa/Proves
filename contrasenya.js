a4e1943f3c0d37645e05290d738bae5c3bcf52d9
AleixSaPa <sapafamilia@gmail.com>
2025-01-12 22:18:08 +0100

    contrasenya.js,index.html,prova01.html

---
+++ contrasenya.js
@@ -0,0 +1,9 @@
+function Script() {
+    resposta_correcta_i_llavors_es_el_aleix="010914"
+    resposta_de_contrasenya_del_usuari=prompt("Quina es la contrasenya")
+    lloc_on_vol_anar
+    if (resposta_de_contrasenya_del_usuari==resposta_correcta_i_llavors_es_el_aleix) {
+        
+    }
+
+}
